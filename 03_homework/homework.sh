cd4250f8699934b8fce146f7a9ea6f446a104f56
IhorKyl <167739687+IhorKyl@users.noreply.github.com>
2024-04-24 17:57:04 -0400

    Update homework.sh
    homework after 1 shell lesson
    task 1-5 done

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -7,14 +7,30 @@
 # On your terminal, input all the commands you have used to create the following:
 
 # 1. How would you create 5 directories? Feel free to use any name for your directories.
+mkdir fld1 fld2 fld3 fld4 fld5
 
 # 2. How would you verify the creation of all 5 directories?
+ls
 
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
+cd fld1/
+touch f.txt
+echo "I love data" > f.txt
+cp f.txt ../fld2/f.txt
+cp f.txt ../fld3/f.txt
+cp f.txt ../fld4/f.txt
+cp f.txt ../fld5/f.txt
 
 # 4. How would you verify the presence of all 5 files?
+cd ../
+ls fld1/*.txt
+ls fld2/*.txt
+ls fld3/*.txt
+ls fld4/*.txt
+ls fld5/*.txt
 
 # 5. How would you append to one of the existing files " and machine learning!"?
+echo " and machine learning!" >> fld1/f.txt
 
 # 6. How would you verify that the text was indeed appended to the existing file?
 
